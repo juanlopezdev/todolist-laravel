3365bc1f34362837c0aea3d86a5579735592fbe2
Juan López <juanlopez.developer@gmail.com>
2018-11-06 22:57:54 -0500

    Creacion de proyecto laravel, migracion activities y creación de rutas y controlador

---
+++ api/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register API routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | is assigned the "api" middleware group. Enjoy building your API!
+  |
+ */
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+  return $request->user();
+});
+
+Route::get('activities', 'ActivityController@index');
+Route::get('activities/{activity}', 'ActivityController@show');
+Route::post('activities', 'ActivityController@store');
+Route::put('activities/{activity}', 'ActivityController@update');
+Route::delete('activities/{activity}', 'ActivityController@delete');
